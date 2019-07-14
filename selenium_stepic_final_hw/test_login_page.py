bffc4f3c0996e4a7cdd694bf20d2a3c2b9dfe0f8
Paparama <fvedernikoff@bk.ru>
2019-07-14 11:59:17 +0300

    add pages crossing

--- selenium_stepic_final_hw/test_login_page.py
+++
@@ -1,9 +0,0 @@
-from .pages.login_page import LoginPage
-
-link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-
-
-def test_is_login_page_login_page(browser):
-    page = LoginPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.should_be_login_page()
