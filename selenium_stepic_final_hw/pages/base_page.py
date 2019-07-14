41e694e5341829ed41e092822f96bf56fc3e0d39
Paparama <fvedernikoff@bk.ru>
2019-07-14 11:43:09 +0300

    add login page checks

--- selenium_stepic_final_hw/pages/base_page.py
+++ selenium_stepic_final_hw/pages/base_page.py
@@ -1,8 +1,9 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.webdriver import RemoteWebDriver
 
 
 class BasePage(object):
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser: RemoteWebDriver, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
